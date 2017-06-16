2793883ab2f032e38385290b9d6f5227c318c4bb
David Hafley <david.hafley@contrastsecurity.com>
2017-06-16 14:01:27 -0500

    Clean up check-service and share with additional roles

--- roles/eop/templates/check-service.sh
+++ roles/eop/templates/check-service.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-URL=http://localhost:8080
-CONTENT=$(curl -sLI $URL | grep HTTP/1.1 | tail -1 | awk {'print $2'})
+URL={{ check_scheme }}://localhost:{{ check_port }}
+CONTENT=$(curl -skLI $URL | grep HTTP/1.1 | tail -1 | awk {'print $2'})
 
 if [ ! -z $CONTENT ] && [ $CONTENT -eq 200 ]
 then
